b08e9f1c10650d1bc60e44e50d617e30849f71f8
SR03 Etudiant <sr03p001@smestaens18n.sme.utc>
2016-03-07 19:00:34 +0100

    update form.js

--- form.js
+++ form.js
@@ -1,13 +1,42 @@
-var nom = document.getElementById("nom"),
+/*var nom = document.getElementById('nom'),
 	prénom = document.getElementById("prenom");
-var alertNom = document.createElement('p');
-
-alertNom.className = "text-danger";
-
+var alertNom = document.createElement('p');*/
+//alertNom.className = "text-danger";
+/*
 nom.addEventListener('onChange', function(e) {
-
+		if()
         //if(e.)
     });
+
+
+*/
+
+
+var $name = $('#nom'),
+	$firstName = $('#prenom');
+
+var isNameCorrect = false,
+	isFirstNameCorrect = false;
+
+$name.change(function(){
+	if($name.val().length >= 2)
+		isNameCorrect = true;
+	else
+		isNameCorrect = false;
+
+	if(!isNameCorrect && !isFirstNameCorrect)
+		alert("Error");
+});
+
+$firstName.change(function(){
+	if($firstName.val().length >= 2)
+		isFirstNameCorrect = true;
+	else
+		isFirstNameCorrect = false;
+
+	if(!isNameCorrect && !isFirstNameCorrect)
+		alert("Error");
+});
 
 
 
