fd6d4f067d9d2a8fc40b629a1928bda716ee926c
SR03 Etudiant <sr03p001@smestaens18n.sme.utc>
2016-03-07 18:16:09 +0100

    update form and creation of th js file

---
+++ form.js
@@ -0,0 +1,13 @@
+var nom = document.getElementById("nom"),
+	prénom = document.getElementById("prenom");
+var alertNom = document.createElement('p');
+
+alertNom.className = "text-danger";
+
+nom.addEventListener('onChange', function(e) {
+
+        //if(e.)
+    });
+
+
+
