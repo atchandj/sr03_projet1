8c77aed40e464fa5792161b8b8a8da6a578b2842
SR03 Etudiant <sr03p001@smestaens18n.sme.utc>
2016-03-14 16:43:26 +0100

    downloaded bootstrap

--- form.js
+++ form.js
@@ -1,17 +1,3 @@
-/*var nom = document.getElementById('nom'),
-	prénom = document.getElementById("prenom");
-var alertNom = document.createElement('p');*/
-//alertNom.className = "text-danger";
-/*
-nom.addEventListener('onChange', function(e) {
-		if()
-        //if(e.)
-    });
-
-
-*/
-
-
 var $name = $('#nom'),
 	$firstName = $('#prenom');
 
--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Page d'accueil du trobinoscope</title>
-		<!-- Latest compiled and minified CSS --><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<!-- Latest compiled and minified JavaScript --><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<script src="bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>		
 		<header>
