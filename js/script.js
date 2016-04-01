d1a078cd52e2edfee90db7ab8245960d80181940
d@ni€| <artchounindaniel@gmail.com>
2016-04-01 21:31:32 +0200

    Optimization of the control of the people form.

--- js/script.js
+++ js/script.js
@@ -3,21 +3,20 @@ var $name = $('#nom'),
 	$selectPere = $('#selectPere'),
 	$selectFils = $('#selectFils');
 
-var errorMsg ='<div id="errorMsg" class="alert alert-danger" role="alert">' + 
+var errorMsg ='<div id="subErrorMsg" class="alert alert-danger" role="alert">' + 
 '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
 '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>' + 
 '<span class="sr-only">Error:</span>Veuillez entrez un nom ou un prénom valide' + 
 '!</div>';
 
-var errorMsgStruct ='<div id="errorMsgStruct" class="alert alert-danger" role="alert">' + 
+var errorMsgStruct ='<div id="subErrorMsgStruct" class="alert alert-danger" role="alert">' + 
 '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
 '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>' + 
 '<span class="sr-only">Error:</span>Vous devez sélctionner une structure' + 
 '!</div>';
 
 function controlPeopleForm(){
-	verificationFormElement($name);
-	verificationFormElement($firstName);
+	verificationFormElement();
 	if($name.parent().hasClass('has-error') || $name.parent().hasClass('has-error')){
 		$('#errorMsg').html(errorMsg);
 		return false;
@@ -34,10 +33,11 @@ function controlStructForm(){
 	return true;
 }
 
-//Vérifis qu'un élément du formulaire des individu est rempli
-function verificationFormElement($formElement){
-	if($firstName.val().length < 1 && $name.val().length < 1){
-		$formElement.parent().addClass("has-error");
+//Vérifies qu'un élément du formulaire des individu est rempli
+function verificationFormElement(){
+	if($firstName.val().length < 2 && $name.val().length < 2){
+		$firstName.parent().addClass("has-error");
+		$name.parent().addClass("has-error");
 	}
 	else{
 		$firstName.parent().removeClass("has-error");
@@ -47,10 +47,14 @@ function verificationFormElement($formElement){
 }
 
 //Listeners
-$name.keydown(function(){
-	verificationFormElement($name);
-});
-$firstName.keydown(function(){
-	verificationFormElement($firstName);
+$name.keyup(function(){
+	verificationFormElement();
 });
 
+$firstName.keyup(function(){
+	verificationFormElement();
+});
+
+$(document).ready(function(){
+    verificationFormElement();
+});
